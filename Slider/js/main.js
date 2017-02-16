28e3f8cad01715a5e63537a30d46d1a690c77822
Victoria Kalashnyk <victoria.kalashnyk@gmail.com>
2017-02-16 17:29:08 +0200

    the latest version of the slider

--- Slider/js/main.js
+++ Slider/js/main.js
@@ -1,12 +1,13 @@
 'use strict';
-	
+
+/* SLIDER */
 var slides = document.getElementsByClassName('slider__block');
 var currentSlide = 0;
 var next = document.getElementsByClassName('slider__nav_next');
 var prev = document.getElementsByClassName('slider__nav_prev');
 var dots = document.getElementsByClassName('dot__link');
 var slideInterval = setInterval(nextSlide, 5000);
-
+ 
 function timeSlide() {
     var time = document.getElementById('time');
     var interval = document.getElementById('result');
@@ -36,13 +37,6 @@ function goToSlide(n) {
 	dots[currentSlide].className = 'dot__link active';	
 }
 
-
-
-
-
-
-
-
 $(function (){
 	/* TASK */
 	$('.task__link').click(function(){
@@ -59,16 +53,36 @@ $(function (){
 		$(".slider").addClass(title);
 	});
 	
-	/* $( "#slider" ).slider({
-        value: 5000,
-        min: 1000,
-        max: 10000,
-        step: 1000,
-        slide: function( event, ui ) {
-            $( "#amount" ).val( ui.value + " ms" );
-        }
-    });
-    $( "#amount" ).val( $( "#slider" ).slider( "value" ) + " ms" ); */
-
+	/* dots */
+	$(".setting__dot").click(function(e) {
+		e.preventDefault();
+		$(".setting__dot").removeClass('selected');
+		$(this).addClass('selected');
+		var title = $(this).attr('title');
+		$(".dot").removeClass().addClass('dot');
+		$(".dot").addClass(title);
+	});
+	
+	/* arrows */
+	$(".setting__arrow").click(function(e) {
+		e.preventDefault();
+		$(".setting__arrow").removeClass('selected');
+		$(this).addClass('selected');
+		var title = $(this).attr('title');
+		$(".arrow").removeClass().addClass('arrow');
+		$(".arrow").addClass(title);
+	});
+	
+	/* effect */
+	$(".setting__effect").click(function(e) {
+		e.preventDefault();
+		$(".setting__effect").removeClass('selected');
+		$(this).addClass('selected');
+		var title = $(this).attr('title');
+		$(".slider__content").removeClass().addClass('slider__content');
+		$(".slider__content").addClass(title);
+	});
+	
+	
 });
 	   
