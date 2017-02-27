e7aca630d3e00a3dbc92e06f6611a04f538b1af6
Victoria <victoria.kalashnyk@gmail.com>
2017-02-27 15:37:15 +0200

    Update main.js
    finished script

--- Game/js/main.js
+++ Game/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 	
 newGame();					
+var xi, yj; // координаты пустой косточки
+var gameBlock = [];
 
 function newGame() {
 	
@@ -9,16 +11,15 @@ function newGame() {
 	while (field.firstChild) {
 		field.removeChild(field.firstChild);
 	}
-
+	
 	// создаем массив массивов
 	var gameBlock = ["one", "two", "three", "four"];
 	gameBlock[0] = [1, 2, 3, 4];
 	gameBlock[1] = [5, 6, 7, 8];
 	gameBlock[2] = [9, 10,11,12];
 	gameBlock[3] = [13,14,15,16];
-	
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
 			if(i + j != 6) {
 				gameBlock[i][j];
 			} else {
@@ -26,113 +27,57 @@ function newGame() {
 			}
 		}; 
 	};
-	console.log(gameBlock);
+	//console.log(gameBlock);
 	
-	// перемишиваем соседние косточки
-	var xi = 3, yj = 3;
-	for(let n = 0; n < 500; ++n)
+	// перемишиваем косточки
+	xi = 3; yj = 3; // координаты пустой косточки при формировании массива
+	for(var n = 0; n < 500; ++n)
 		switch(Math.round(3*Math.random())){
 			case 0: if(xi != 0) swap(gameBlock,xi,yj,--xi,yj); break; // top
 			case 1: if(yj != 3) swap(gameBlock,xi,yj,xi,++yj); break; // right
-			case 2: if(xi != 3) swap(gameBlock,xi,yj,++xi,yj); break; // bootom
+			case 2: if(xi != 3) swap(gameBlock,xi,yj,++xi,yj); break; // bottom
 			case 3: if(yj != 0) swap(gameBlock,xi,yj,xi,--yj);        // left
 		}
-	console.log(xi,yj);
+	//console.log(xi,yj); // новые координаты пустой косточки после перемешивания
 	
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
 			var block = document.createElement("div");	
 			field.appendChild(block);
 			if(gameBlock[i][j] == '') {
-				block.className = "empty";
+				block.id = "block" + i + j;
+				block.setAttribute("onclick", "move(" + i + "," + j + ")");
 				block.innerHTML = (gameBlock[i][j]);
 			} else {
-				block.className = "block";
+				block.id = "block" + i + j;
+				block.setAttribute("onclick", "move(" + [i] + "," + [j] + ")");
 				block.innerHTML += (gameBlock[i][j]);
+				block.style.background = "#525461";
 			}
 		}; 
 	};
 }
 
+function move(i,j) {
+	if((i == xi && Math.abs(j - yj) == 1) || (j == yj && Math.abs(i - xi) == 1)) {
+		var empty = document.getElementById('block' + xi + yj);
+		var elem = document.getElementById('block' + i + j);
+		empty.innerHTML = elem.innerHTML;
+		empty.style.background = "#525461";
+		elem.innerHTML = "";
+		elem.style.background = "none";
+		xi = i;
+		yj = j;
+		// console.log(xi,yj); // координаты новой пустой косточки после передвижения косточки
+	}
+}
+
 function swap(gameBlock,i1,j1,i2,j2) {
-	// [i1][j1] - координаты косточки
+	// [i1][j1] - координаты пустой косточки
 	// [i2][j2] - координаты соседней косточки
-	let sub = gameBlock[i1][j1];
+	var sub = gameBlock[i1][j1];
 	gameBlock[i1][j1] = gameBlock[i2][j2];
 	gameBlock[i2][j2] = sub;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// создаем массив массивов
-/* var number = [];
-for(let i = 0; i < 4; i++){
-	number[i] = []
-	for(let j = 0; j < 4; j++){
-		
-		var block = document.createElement("div");
-        block.className = "block";
-		content.appendChild(block);
-		
-		if(i + j != 6) {
-			block.innerHTML += (number[i][j] = i*4 + j + 1);
-		} else {
-			block.innerHTML = (number[i][j] = "");
-		}
-	}
-	document.write("<br>");
-}
-
-var ei = 3;
-var ej = 3;
-for(let i = 0; i < 1600; ++i)
-	switch(Math.round(3*Math.random())){
-		case 0: if(ei != 0) swap(number,ei,ej,--ei,ej); break; // up
-		case 1: if(ej != 3) swap(number,ei,ej,ei, ++ej); break; // right
-		case 2: if(ei != 3) swap(number,ei,ej,++ei,ej); break; // down
-		case 3: if(ej != 0) swap(number,ei,ej,ei,--ej); // left
-	} */
-
-	/* var numberBlock = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; */
-	
-	/* $.each(numberBlock, function(index, value){
-		var ind = ++index;
-		$('.content').append('<div class="block_' + index + '">' + value + '</div>');
-    }); */
-
-	/* // премешивание массива в случайном порядке
-	function getRandomInt(a, b) {
-		return Math.random() - 0.5;
-	}
-	var numberRandom = numberBlock.sort(getRandomInt);
-	
-	// перебор массива
-	$.each(numberRandom, function(index, value){
-		var ind = ++index;
-		$('.content').append('<div class="block_' + index + '">' + value + '</div>');
-		console.log(index + ' ' + value);
-    }); */
-	
 
 
